db60e3b6dc785bdd55a3b4e32dd006aae0989753
grich said <said.grich@edu.uca.ma>
2021-12-25 10:27:34 -0800

    v1.03

--- GooglemapsScript.py
+++ GooglemapsScript.py
@@ -1,12 +1,10 @@
 import json
 
 from selenium import webdriver
-from selenium.common.exceptions import StaleElementReferenceException
-from selenium.webdriver import Keys
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 
